26895267d2b733118a8f438f1a23ef1d2813838e
GeorgiPasev <pasev81@gmail.com>
2026-03-02 01:44:02 +0200

    Added Departments model

---
+++ Models/Departments.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Razor.Language.Intermediate;
+using System.ComponentModel.DataAnnotations;
+namespace SampleCrudMVC.Models
+{
+    public class Departments
+    {
+        
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage ="Department name is required")]
+        public string Name { get; set; }
+
+         public string Description { get; set; }   
+    }
+}
