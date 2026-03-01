edb40e591f667d0473a421aae5ce98707da2e7f9
GeorgiPasev <pasev81@gmail.com>
2026-03-01 15:02:56 +0200

    Added phone number validation

--- Models/Employee.cs
+++ Models/Employee.cs
@@ -6,6 +6,8 @@
         public string? FirstName { get; set; } 
         public string? MiddleName { get; set; }
         public string? LastName { get; set; }
+        [StringLength(10, MinimumLength = 10, ErrorMessage = "Phone number must be 10 symbols")]
+        [Phone]
         public string? PhoneNumber { get; set; }
         public string? EmailAdress { get; set; }
         public string? Gender { get; set; }
